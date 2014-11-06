f48c9fd79fe5c858f28c367cac37315ce6c64b1a
belinyak <belinyakm@gmail.com>
2014-11-06 14:08:29 +0100

    init

---
+++ LaborAdatbazisKezelo/Program.cs
@@ -0,0 +1,20 @@
+﻿using LaborAdatbaziKezelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaborAdatbazisKezelo
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form_AdatbázisKezelő());
+        }
+    }
+}
