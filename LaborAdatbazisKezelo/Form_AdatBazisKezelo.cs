460b65c3038dfa0029c86e192d0d1afb689a9c10
belinyak <belinyakm@gmail.com>
2014-11-19 23:16:19 +0100

    hordo törlés

--- LaborAdatbazisKezelo/Form_AdatBazisKezelo.cs
+++ LaborAdatbazisKezelo/Form_AdatBazisKezelo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
@@ -10,14 +11,32 @@ using System.Windows.Forms;
 
 namespace LaborAdatbaziKezelo
 {
+    public struct Hordó
+    {
+        public string termékkód;
+        public string id;
+        public int db;
+
+
+        public Hordó(string _termékkód, string _id, int _db)
+        {
+            termékkód = _termékkód;
+            id = _id;
+            db= _db;
+        }
+    }
+
     class Form_AdatbázisKezelő: Form
     {
+        private SqlConnection laborconnection;
+        private SqlConnection marillenconnection;
         public static string server = ".\\SQLEXPRESS";
         public static string labor_database = "Labor";
+        public static string marillen_database = "marillen2014";
         public static string sql_username = "labor";
         public static string sql_password = "labor";
+        public static bool integrated_security = false;
 
-        private SqlConnection laborconnection;
 
         TextBox[] rekordok;
         TextBox[] típusok;
@@ -29,6 +48,12 @@ namespace LaborAdatbaziKezelo
             Configurate();
             string LaborConnectionString = @"Data Source=" + server + ";Initial Catalog=" +
                            labor_database + ";User ID=" + sql_username + ";Password=" + sql_password + ";Integrated Security=true;";
+            string MarillenConnectionString = @"Data Source=" + server + ";Initial Catalog=" + marillen_database + ";" +
+                (integrated_security ? "Integrated Security=true;" : "User ID=" + sql_username + ";Password=" + sql_password + ";");
+
+            marillenconnection = new SqlConnection(MarillenConnectionString);
+            
+
             laborconnection = new SqlConnection(LaborConnectionString);
             laborconnection.Open();
             InitilizeContent();
@@ -105,6 +130,14 @@ namespace LaborAdatbaziKezelo
             minbiz.Size = new System.Drawing.Size(96, 32);
             minbiz.Location = new Point(ClientRectangle.Width - minbiz.Size.Width - 92 - rendben.Width, ClientRectangle.Height - minbiz.Size.Height - 16);
             minbiz.Click += minbiz_Click;
+
+
+            Button hordo = new Button();
+            hordo.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+            hordo.Text = "hordo";
+            hordo.Size = new System.Drawing.Size(96, 32);
+            hordo.Location = new Point(ClientRectangle.Width - hordo.Size.Width -16, ClientRectangle.Height - hordo.Size.Height - 64);
+            hordo.Click += hordo_Click;
             
             Controls.Add(label_név);
             Controls.Add(label_típus);
@@ -112,7 +145,63 @@ namespace LaborAdatbaziKezelo
             Controls.Add(rendben);
             Controls.Add(csere);
             Controls.Add(minbiz);
+            Controls.Add(hordo);
         }
+
+    
+        private void hordo_Click(object sender, EventArgs e)
+        {
+            Form_Hordók hordók = new Form_Hordók();
+            hordók.ShowDialog();
+
+            /*
+            List<hordo> hordok = new List<hordo>();
+            laborconnection.Open();
+            SqlCommand command = laborconnection.CreateCommand();
+            command.CommandText = "SELECT VITEKO,VISARZ FROM L_VIZSLAP GROUP BY VITEKO, VISARZ ORDER BY VITEKO, VISARZ ";
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                hordok.Add(new hordo(reader.GetString(0), reader.GetString(1)));
+                //Console.WriteLine(reader.GetString(0) + " " + reader.GetString(1));
+            }
+
+            command.Dispose();
+            laborconnection.Close();
+            reader.Close();
+            List<string> asd = new List<string>();
+            int c = 0;
+            foreach (hordo item in hordok)
+            {
+                string iPROD_ID = "12" + item.viteko.Substring(0, 2) + "01" + item.viteko[2];
+                marillenconnection.Open();
+                command = marillenconnection.CreateCommand();
+                command.CommandText = "SELECT tetelek.prod_id, tetelek.qty, tetelek.time_ FROM tetelek" +
+                                            " INNER JOIN folyoprops ON tetelek.serial_nr=folyoprops.serial_nr" +
+                                            " WHERE left(tetelek.prod_id,7) = '" + iPROD_ID + "' AND folyoprops.code= '3' AND folyoprops.propstr = '" + item.visarz + "'  ORDER BY tetelek.prod_id;";
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    c++;
+                    if (!(asd.Contains(reader.GetString(0).Substring(reader.GetString(0).Length - 4))))
+                    {
+                        asd.Add(reader.GetString(0).Substring(reader.GetString(0).Length - 4));
+                    }
+                    else
+                    {
+                        Console.WriteLine( item.viteko +" " +  item.visarz);// +  reader.GetString(0).Substring(reader.GetString(0).Length - 4));//+ " " + reader.GetDecimal(1) + reader.GetDateTime(2).ToString());
+                    }
+                }
+                Console.WriteLine("c: " + c);
+
+                reader.Close();
+                marillenconnection.Close();
+            }
+            */
+        }
+
+        
 
         void minbiz_Click(object sender, EventArgs e)
         {
@@ -184,6 +273,8 @@ namespace LaborAdatbaziKezelo
             laborconnection.Close();
         }
 
+      
+
         void rendben_Click(object sender, EventArgs e)
         {
             laborconnection.Open();
@@ -227,8 +318,10 @@ namespace LaborAdatbaziKezelo
                                 {
                                     case "server": server = arguments[1]; break;
                                     case "labor_database": labor_database = arguments[1]; break;
+                                    case "marillen_database": marillen_database = arguments[1]; break;
                                     case "sql_username": sql_username = arguments[1]; break;
                                     case "sql_password": sql_password = arguments[1]; break;
+                                    case "integrated_security": integrated_security = true; break;
                                 }
                             }
                     }
@@ -238,5 +331,185 @@ namespace LaborAdatbaziKezelo
             }
             catch { }
         }
+
+        public sealed class Form_Hordók : Form
+        {
+            private SqlConnection laborconnection;
+
+            protected DataGridView table;
+            protected DataTable data;
+            public Form_Hordók()
+            {
+                string LaborConnectionString = @"Data Source=" + server + ";Initial Catalog=" +
+                           labor_database + ";User ID=" + sql_username + ";Password=" + sql_password + ";Integrated Security=true;";
+               
+              
+                laborconnection = new SqlConnection(LaborConnectionString);
+                
+
+                InitializeForm();
+                InitializeContent();
+                InitializeData();
+            }
+            private void InitializeForm()
+            {
+                Text = "Hordók";
+                ClientSize = new Size(512, 568);
+                MinimumSize = ClientSize;
+                StartPosition = FormStartPosition.CenterScreen;
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            }
+
+            public List<Hordó> Hordók()
+            {
+                List<Hordó> value = new List<Hordó>();
+
+                laborconnection.Open();
+                SqlCommand command = laborconnection.CreateCommand();
+                command.CommandText = "SELECT HOTEKO, HOSZAM, COUNT(*) AS DB FROM L_HORDO GROUP BY HOTEKO, HOSZAM ORDER BY DB DESC";
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    value.Add(new Hordó(reader.GetString(0), reader.GetString(1), reader.GetInt32(2)));
+                }
+
+                command.Dispose();
+                laborconnection.Close();
+                return value;
+            }
+
+            private void InitializeContent()
+            {
+                table = new DataGridView();
+                table.Dock = DockStyle.Left;
+                table.RowHeadersVisible = false;
+                table.AllowUserToResizeRows = false;
+                table.AllowUserToResizeColumns = false;
+                table.AllowUserToAddRows = false;
+                table.Width = 250;
+                table.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                table.ReadOnly = true;
+                table.DataBindingComplete += table_DataBindingComplete;
+                table.DataSource = CreateSource();
+
+                Button törlés = new Button();
+                törlés.Anchor = AnchorStyles.Left | AnchorStyles.Bottom;
+                törlés.Text = "Törlés";
+                törlés.Size = new System.Drawing.Size(96, 32);
+                törlés.Location = new System.Drawing.Point(table.Location.X + table.Size.Width + 16, ClientRectangle.Height - 32 - 16);
+                törlés.Click += törlés_Click;
+
+                Controls.Add(table);
+                Controls.Add(törlés);
+
+            }
+
+            public struct Hordó2
+            {
+                public string termékkód;
+                public string sarzs;
+                public string id;
+                public int? foglalás_száma;
+                public string gyártási_év;
+                public decimal mennyiség;
+                public string time;
+
+                public Hordó2(string _termékkód, string _sarzs, string _id, int? _foglalás_száma, string _gyártási_év, decimal _mennyiség, string _time)
+                {
+                    termékkód = _termékkód;
+                    sarzs = _sarzs;
+                    id = _id;
+                    foglalás_száma = _foglalás_száma;
+                    gyártási_év = _gyártási_év;
+                    mennyiség = _mennyiség;
+                    time = _time;
+                }
+            }
+            public static T? GetNullable<T>(SqlDataReader _reader, int _column) where T : struct
+            {
+                if (!_reader.IsDBNull(_column))
+                    return _reader.GetFieldValue<T>(_column);
+                return null;
+            }
+
+            private void törlés_Click(object sender, EventArgs e)
+            {
+
+                laborconnection.Open();
+                SqlCommand command = laborconnection.CreateCommand();
+                command.CommandText = "CREATE TABLE temp(HOTEKO varchar(10), HOSARZ varchar(10), HOSZAM varchar(10), FOSZAM int, VIGYEV varchar(10), HOQTY decimal(14, 2), HOTIME char(30));";
+                command.ExecuteNonQuery();
+                command.Dispose();
+
+                List<Hordó2> hordók = new List<Hordó2>();
+                command = laborconnection.CreateCommand();
+                command.CommandText = "SELECT DISTINCT HOTEKO, HOSARZ, HOSZAM, FOSZAM, VIGYEV, HOQTY, HOTIME FROM L_HORDO ORDER BY HOTEKO, HOSARZ, HOSZAM";
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    hordók.Add(new Hordó2(reader.GetString(0), reader.GetString(1), reader.GetString(2), GetNullable<int>(reader, 3), reader.GetString(4), reader.GetDecimal(5), reader.GetString(6)));
+                }
+
+                command.Dispose();
+                reader.Close();
+
+               command = laborconnection.CreateCommand();
+                foreach (Hordó2 item in hordók)
+                {
+                    command.CommandText += "INSERT INTO temp (HOTEKO, HOSARZ, HOSZAM, VIGYEV, HOQTY, HOTIME) VALUES('" + item.termékkód + "','" + item.sarzs + "','" +
+                        item.id + "','" +item.gyártási_év + "', " + item.mennyiség.ToString("F2").Replace(',', '.') + ", '" + item.gyártási_év + "');";
+                }
+                command.ExecuteNonQuery();
+                command.Dispose();
+
+                command = laborconnection.CreateCommand();
+                command.CommandText = "DROP TABLE L_HORDO; EXEC sp_rename 'temp', 'L_HORDO';";
+                 command.ExecuteNonQuery();
+                command.Dispose();
+
+                laborconnection.Close();
+                Close();
+                   //"INSERT INTO temp(HOTEKO,HOSARZ, HOSZAM, FOSZAM , VIGYEV , HOQTY , HOTIME) values ( (SELECT DISTINCT HOTEKO, HOSARZ, HOSZAM, FOSZAM, VIGYEV, HOQTY, HOTIME FROM L_HORDO));";
+            }
+
+            private void table_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+            {
+                table.DataBindingComplete -= table_DataBindingComplete;
+                table.Columns[0].Width = 83;
+                table.Columns[1].Width = 83;
+                table.Columns[2].Width = 83;
+            }
+            private void InitializeData()
+            {
+
+            }
+
+
+            private DataTable CreateSource()
+            {
+                data = new DataTable();
+
+                data.Columns.Add(new DataColumn("Termékkód", System.Type.GetType("System.String")));
+                data.Columns.Add(new DataColumn("ID", System.Type.GetType("System.String")));
+                data.Columns.Add(new DataColumn("DB", System.Type.GetType("System.Int32")));
+
+                List<Hordó> hordók = Hordók();
+
+                foreach (Hordó item in hordók)
+                {
+                    if(item.db > 1)
+                    {
+                        DataRow row = data.NewRow();
+                        row[0] = item.termékkód;
+                        row[1] = item.id;
+                        row[2] = item.db;
+                        data.Rows.Add(row);
+                    }
+                }
+
+                return data;
+            }
+        }
+
     }
 }
